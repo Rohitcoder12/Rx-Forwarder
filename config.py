8d6cf9c2e3041959a03459e33aa96f17a8374350
Rohitcoder12 <rohitgandu91@gmail.com>
2025-07-10 18:27:41 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+api_id = 1234567  # Replace with your Telegram API ID
+api_hash = 'your_api_hash'  # Replace with your Telegram API hash
+bot_token = 'your_bot_token' # Replace with your bot token
+db_file = 'users.db'
+# Add other configuration settings as needed
