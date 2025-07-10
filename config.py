b60b378bbbed1a8ac0b835a458a977d4e5f405bf
Rohitcoder12 <rohitgandu91@gmail.com>
2025-07-10 18:31:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # config.py
-api_id = 1234567  # Replace with your Telegram API ID
-api_hash = 'your_api_hash'  # Replace with your Telegram API hash
-bot_token = 'your_bot_token' # Replace with your bot token
+import os
+
+api_id = int(os.environ.get('API_ID'))
+api_hash = os.environ.get('API_HASH')
+bot_token = os.environ.get('BOT_TOKEN')
 db_file = 'users.db'
-# Add other configuration settings as needed
